32e0753a97bec48027cce7062134a8309b526765
watarukura <kurashima-wataru@dmm.com>
2024-11-20 10:43:15 +0900

    chore: mise activate

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -16,6 +16,7 @@ fish_add_path "$HOME/.local/share/aquaproj-aqua/bin/"
 fish_add_path "/usr/local/opt/mysql-client@8.0/bin"
 fish_add_path "/opt/homebrew/opt/mysql@8.0/bin"
 fish_add_path "/opt/homebrew/sbin"
+fish_add_path "$HOME/.local/share/mise/shims"
 set -x EDITOR 'vim'
 
 # less
